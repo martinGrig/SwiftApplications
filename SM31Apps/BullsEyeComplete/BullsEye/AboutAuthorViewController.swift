7b2c1113a33b465fbd46325473f8950058867c5c
Student <student@ISSDs-MacBook-Pro-6.local>
2019-09-08 13:27:00 +0200

    Bull's Eye game completed

---
+++ SM31Apps/BullsEyeComplete/BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Student on 07/09/2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+}
